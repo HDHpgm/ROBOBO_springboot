4df719632785a74e99a56d9176f2a2168dfeb880
HDHpgm <gb78@naver.com>
2021-11-27 17:30:25 +0900

    최신

--- src/main/java/com/robobo/rbb_springboot/controller/MqttController.java
+++ src/main/java/com/robobo/rbb_springboot/controller/MqttController.java
@@ -18,13 +18,27 @@ public class MqttController {
 	MqttGateway mqtGateway;
 
 
-	@PostMapping("/sendPowerOn")
-	public String publish(@RequestBody String mqttMessage){
-		
+	@PostMapping("/sendPowerOn1")
+	public String publish1(@RequestBody String mqttMessage){
+
 		try {
 	//		System.out.println(mqttMessage);
 	//		JsonObject convertObject = new Gson().fromJson(mqttMessage, JsonObject.class);
 			mqtGateway.senToMqtt("1");
+			return "전원 켜짐";
+		}catch(Exception ex) {
+			System.out.println("실패 "+ex);
+			return "실패라빠빠..";
+		}
+	}
+
+	@PostMapping("/sendPowerOn2")
+	public String publish2(@RequestBody String mqttMessage){
+
+		try {
+			//		System.out.println(mqttMessage);
+			//		JsonObject convertObject = new Gson().fromJson(mqttMessage, JsonObject.class);
+			mqtGateway.senToMqtt("2");
 			return "전원 켜짐";
 		}catch(Exception ex) {
 			System.out.println("실패 "+ex);
